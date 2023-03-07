c1d41853649ce3f85ca75fd46c9e98801d896355
dzakyy04 <dzakylinggau@gmail>
2023-03-07 19:14:18 +0700

    membuat halaman about dan membuat route group

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,16 +13,23 @@ use App\Http\Controllers\RegisterController;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('home.index');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login.auth');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::group(['middleware' => 'guest'], function () {
+    // Login Routes
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login.auth');
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    // Register Routes
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+});
 
+Route::group(['middleware' => 'auth'], function () {
+    // Logout Route
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
